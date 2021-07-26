115bd81b178a6acc4554dc702d5c11bb37955235
fateme <fateme3611@yahoo.com>
2021-07-26 18:27:26 -0400

    test

--- head.js
+++ head.js
@@ -1,11 +1,13 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
-  } else if (actual !== expected) {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
-  }
-};
+//const assertEqual = function(actual, expected) {
+  //if (actual === expected) {
+    //console.log("Assertion Passed: " + actual + " === " + expected);
+  //} else if (actual !== expected) {
+    //console.log("Assertion Failed: " + actual + " !== " + expected);
+ // }
+//};
+
+const assertEqual = require ('./assertEqual');
 const head = function(arr) {
   return arr[0];
 };

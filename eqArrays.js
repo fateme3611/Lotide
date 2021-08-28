3a479f3bd49ad7d76f7d013cc5c0f1fec09a47d1
fateme <fateme3611@yahoo.com>
2021-08-27 20:51:58 -0400

    correct errors in map.js and egarray.js

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@ const eqArrays = function (arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return false;
     }
-    for (let i = 0; i < arr1.lenght; i++) {
+    for (let i = 0; i < arr1.length; i++) {
         if (arr1[i] !== arr2[i]) {
             return false;
         }
--- map.js
+++ map.js
@@ -2,9 +2,8 @@ const map = function (array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
-    return results;
   };
+  return results;
 }
-
 module.exports = map;
 
